f245fe28ec04ba5dd253d2c77ae87ed8e983ee04
Celeste <yuan.zx@outlook.com>
2020-03-26 13:27:56 +0800

    refactor: use lib

--- Queue.vhdl
+++ Queue.vhdl
@@ -1,42 +1,49 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-USE ieee.std_logic_arith.ALL;
-USE ieee.std_logic_unsigned.ALL;
 
 ENTITY Queue IS
+  GENERIC (
+    RAM_WIDTH : NATURAL := 8;
+    RAM_DEPTH : NATURAL := 8
+  );
   PORT (
     clock : IN std_logic;
-
-    -- operate
     reset : IN std_logic;
-    pop : IN std_logic;
+
+    -- Write port
     push : IN std_logic;
+    data_in : IN std_logic_vector(RAM_WIDTH - 1 DOWNTO 0);
 
-    -- state
+    -- Read port
+    pop : IN std_logic;
+    enable_read : OUT std_logic;
+    data_out : OUT std_logic_vector(RAM_WIDTH - 1 DOWNTO 0);
+
+    -- Flags
     empty : OUT std_logic;
+    empty_next : OUT std_logic;
     full : OUT std_logic;
+    full_next : OUT std_logic;
 
-    -- data
-    data_in : IN std_logic_vector(7 DOWNTO 0); -- depth
-    data_out : OUT std_logic_vector(7 DOWNTO 0) -- depth
+    -- The number of elements in the FIFO
+    fill_count : OUT INTEGER RANGE RAM_DEPTH - 1 DOWNTO 0
   );
 END Queue;
 
-ARCHITECTURE arch OF Queue IS
-  SUBTYPE word IS std_logic_vector(7 DOWNTO 0);
-  TYPE queue_array IS ARRAY(7 DOWNTO 0) OF word;
-  SIGNAL queue : queue_array;
+ARCHITECTURE rtl OF Queue IS
 
-  TYPE states IS(init, idle, state_pop, state_push);
-  SIGNAL present_state : states;
-  SIGNAL next_state : states;
+  TYPE ram_type IS ARRAY (0 TO RAM_DEPTH - 1) OF std_logic_vector(data_in'RANGE);
+  SIGNAL ram : ram_type;
 
-  SIGNAL s_full : std_logic := '0';
-  SIGNAL s_empty : std_logic := '0';
+  SUBTYPE index_type IS INTEGER RANGE ram_type'RANGE;
+  SIGNAL head : index_type;
+  SIGNAL tail : index_type;
 
-  SIGNAL front : INTEGER RANGE 0 TO 7 := 0;
-  SIGNAL rear : INTEGER RANGE 0 TO 7 := 0;
+  SIGNAL empty_i : std_logic;
+  SIGNAL full_i : std_logic;
+  SIGNAL fill_count_i : INTEGER RANGE RAM_DEPTH - 1 DOWNTO 0;
 
+  -- Increment and wrap
   PROCEDURE incr(SIGNAL index : INOUT index_type) IS
   BEGIN
     IF index = index_type'high THEN
@@ -45,84 +52,75 @@ ARCHITECTURE arch OF Queue IS
       index <= index + 1;
     END IF;
   END PROCEDURE;
+
 BEGIN
-  -- clock trigger
-  PROCESS (clock)
+  -- Copy internal signals to output
+  empty <= empty_i;
+  full <= full_i;
+  fill_count <= fill_count_i;
+
+  -- Set the flags
+  empty_i <= '1' WHEN fill_count_i = 0 ELSE
+    '0';
+  empty_next <= '1' WHEN fill_count_i <= 1 ELSE
+    '0';
+  full_i <= '1' WHEN fill_count_i >= RAM_DEPTH - 1 ELSE
+    '0';
+  full_next <= '1' WHEN fill_count_i >= RAM_DEPTH - 2 ELSE
+    '0';
+
+  -- Update the head pointer in write
+  PROC_HEAD : PROCESS (clock)
   BEGIN
-    IF (reset = '1') THEN
-      present_state <= init;
-    ELSIF (clock'event AND clock = '1') THEN
-      present_state <= next_state;
+    IF rising_edge(clock) THEN
+      IF reset = '1' THEN
+        head <= 0;
+      ELSE
+
+        IF push = '1' AND full_i = '0' THEN
+          incr(head);
+        END IF;
+
+      END IF;
     END IF;
   END PROCESS;
 
-  -- state change
-  PROCESS (present_state)
+  -- Update the tail pointer on read and pulse valid
+  PROC_TAIL : PROCESS (clock)
   BEGIN
-    CASE present_state IS
-      WHEN init =>
-        next_state <= idle;
+    IF rising_edge(clock) THEN
+      IF reset = '1' THEN
+        tail <= 0;
+        enable_read <= '0';
+      ELSE
+        enable_read <= '0';
 
-      WHEN state_pop =>
-        next_state <= idle;
-
-      WHEN state_push =>
-        next_state <= idle;
-
-      WHEN idle =>
-        IF push = '1' THEN
-          next_state <= state_push;
-        ELSIF pop = '1' THEN
-          next_state <= state_pop;
-        ELSE
-          next_state <= idle;
+        IF pop = '1' AND empty_i = '0' THEN
+          incr(tail);
+          enable_read <= '1';
         END IF;
 
-      WHEN OTHERS =>
-        next_state <= idle;
-    END CASE;
+      END IF;
+    END IF;
   END PROCESS;
 
-  -- state events
-  PROCESS (present_state)
+  -- Write to and read from the RAM
+  PROC_RAM : PROCESS (clock)
   BEGIN
-    s_full <= '0';
-    s_empty <= '0';
-
-    CASE present_state IS
-      WHEN init =>
-        front <= 0;
-        rear <= 0;
-        FOR i IN 7 DOWNTO 0 LOOP
-          queue(i) <= (OTHERS => '0'); -- aggregate
-        END LOOP;
-
-      WHEN state_pop =>
-        IF (front /= rear) THEN
-          data_out <= queue(rear);
-          rear <= rear + 1;
-        ELSE
-          s_empty <= '1';
-        END IF;
-
-      WHEN state_push =>
-        IF (front + 1 /= rear) THEN
-          queue(front) <= data_in;
-          incr(front);
-        ELSE
-          s_full <= '1';
-        END IF;
-
-      WHEN idle =>
-        NULL;
-
-    END CASE;
+    IF rising_edge(clock) THEN
+      ram(head) <= data_in;
+      data_out <= ram(tail);
+    END IF;
   END PROCESS;
 
-  -- full/empty update
-  PROCESS (s_full, s_empty)
+  -- Update the fill count
+  PROC_COUNT : PROCESS (head, tail)
   BEGIN
-    full <= s_full;
-    empty <= s_empty;
+    IF head < tail THEN
+      fill_count_i <= head - tail + RAM_DEPTH;
+    ELSE
+      fill_count_i <= head - tail;
+    END IF;
   END PROCESS;
-END arch;
+
+END ARCHITECTURE;
