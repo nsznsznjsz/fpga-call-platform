d45c0445411df88b1c76a5927dddcbbbce85a5d4
Celeste <yuan.zx@outlook.com>
2020-04-10 15:13:02 +0800

    refactor: update flag docs

--- Service.vhd
+++ Service.vhd
@@ -25,8 +25,23 @@ ENTITY Service IS
 END Service;
 
 ARCHITECTURE arch OF Service IS
+  -- If you are updating flags, you need: 
+  -- 1. Update README
+  -- 2. Synchronous update `Waiting`, `Service` and `Decoder`
+  -- 3. Check groups flag is right, vip_flags = bit_xor(vip, group)
+  --
+  -- I try my best to maintain the consistency of the flags, but we
+  -- need a better way to replace the manual operation. 
+
   CONSTANT FLAG_SCREEN_WAITING : std_logic_vector(1 DOWNTO 0) := "10";
   CONSTANT FLAG_SCREEN_SERVICE : std_logic_vector(1 DOWNTO 0) := "11";
+
+  CONSTANT FLAG_GROUP_FREE : std_logic_vector(3 DOWNTO 0) := "0000";
+  CONSTANT FLAG_GROUP_A : std_logic_vector(3 DOWNTO 0) := "0001";
+  CONSTANT FLAG_GROUP_B : std_logic_vector(3 DOWNTO 0) := "0010";
+  CONSTANT FLAG_GROUP_VIPA : std_logic_vector(3 DOWNTO 0) := "1001";
+  CONSTANT FLAG_GROUP_VIPB : std_logic_vector(3 DOWNTO 0) := "1001";
+
   CONSTANT FLAG_ERROR_FREE : std_logic_vector(1 DOWNTO 0) := "00";
   CONSTANT FLAG_ERROR_QUEUE_EMPTY : std_logic_vector(1 DOWNTO 0) := "10";
   CONSTANT FLAG_ERROR_QUEUE_FULL : std_logic_vector(1 DOWNTO 0) := "11";
@@ -48,7 +63,7 @@ ARCHITECTURE arch OF Service IS
   BEGIN
     IF (enable = '1') THEN
       next_state <= s_next;
-      ELSE
+    ELSE
       next_state <= s_wait;
     END IF;
   END PROCEDURE;
@@ -99,7 +114,13 @@ BEGIN
     pull <= '0';
 
     CASE present_state IS
-      WHEN idle => NULL;
+      WHEN idle =>
+        data <= (OTHERS => '0');
+        data <=
+          FLAG_SCREEN_SERVICE
+          & FLAG_GROUP_FREE
+          & FLAG_ERROR_UNKNOWN
+          & data(RAM_WIDTH - 9 DOWNTO 0);
 
       WHEN pulling =>
         pull <= '1';
--- Waiting.vhd
+++ Waiting.vhd
@@ -25,8 +25,23 @@ ENTITY Waiting IS
 END Waiting;
 
 ARCHITECTURE arch OF Waiting IS
+  -- If you are updating flags, you need: 
+  -- 1. Update README
+  -- 2. Synchronous update `Waiting`, `Service` and `Decoder`
+  -- 3. Check groups flag is right, vip_flags = bit_xor(vip, group)
+  --
+  -- I try my best to maintain the consistency of the flags, but we
+  -- need a better way to replace the manual operation. 
+
   CONSTANT FLAG_SCREEN_WAITING : std_logic_vector(1 DOWNTO 0) := "10";
   CONSTANT FLAG_SCREEN_SERVICE : std_logic_vector(1 DOWNTO 0) := "11";
+
+  CONSTANT FLAG_GROUP_FREE : std_logic_vector(3 DOWNTO 0) := "0000";
+  CONSTANT FLAG_GROUP_A : std_logic_vector(3 DOWNTO 0) := "0001";
+  CONSTANT FLAG_GROUP_B : std_logic_vector(3 DOWNTO 0) := "0010";
+  CONSTANT FLAG_GROUP_VIPA : std_logic_vector(3 DOWNTO 0) := "1001";
+  CONSTANT FLAG_GROUP_VIPB : std_logic_vector(3 DOWNTO 0) := "1001";
+
   CONSTANT FLAG_ERROR_FREE : std_logic_vector(1 DOWNTO 0) := "00";
   CONSTANT FLAG_ERROR_QUEUE_EMPTY : std_logic_vector(1 DOWNTO 0) := "10";
   CONSTANT FLAG_ERROR_QUEUE_FULL : std_logic_vector(1 DOWNTO 0) := "11";
@@ -48,7 +63,7 @@ ARCHITECTURE arch OF Waiting IS
   BEGIN
     IF (enable = '1') THEN
       next_state <= s_next;
-      ELSE
+    ELSE
       next_state <= s_wait;
     END IF;
   END PROCEDURE;
