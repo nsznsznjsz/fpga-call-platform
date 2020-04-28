91a8291391a4f8d37b8e1b5697558a3220169734
Celeste <yuan.zx@outlook.com>
2020-04-28 09:01:58 +0800

    fix(FD): work with n=1

--- FD.vhd
+++ FD.vhd
@@ -14,16 +14,29 @@ ENTITY FD IS
   );
 END FD;
 
+-- BUG: 占空比不为 50%, 而是 N-1/N+1
 ARCHITECTURE arch OF FD IS
-  SIGNAL cnt_up, cnt_down : INTEGER RANGE 0 TO 2 * N - 1;
+  SIGNAL cnt_up, cnt_down : INTEGER RANGE 0 TO N - 1;
+  SIGNAL s_clock_div : std_logic;
 BEGIN
 
-  PROCESS (cnt_up, cnt_down)
+  PROCESS (clock, reset, s_clock_div)
+  BEGIN
+    IF (reset = '1') THEN
+      clock_div <= '0';
+    ELSIF (N = 1) THEN
+      clock_div <= clock;
+    ELSE
+      clock_div <= s_clock_div;
+    END IF;
+  END PROCESS;
+
+  PROCESS (clock, cnt_up, cnt_down)
   BEGIN
     IF (cnt_up + cnt_down < N) THEN
-      clock_div <= '0';
+      s_clock_div <= '0';
     ELSE
-      clock_div <= '1';
+      s_clock_div <= '1';
     END IF;
   END PROCESS;
 
@@ -32,7 +45,7 @@ BEGIN
     IF (reset = '1') THEN
       cnt_up <= 0;
     ELSIF (clock'event AND clock = '1') THEN --上升沿计数
-      IF (cnt_up < 2 * N - 1) THEN
+      IF (cnt_up < N - 1) THEN
         cnt_up <= cnt_up + 1;
       ELSE
         cnt_up <= 0;
@@ -45,7 +58,7 @@ BEGIN
     IF (reset = '1') THEN
       cnt_down <= 0;
     ELSIF (clock'event AND clock = '0') THEN --下降沿计数
-      IF (cnt_down < 2 * N - 1) THEN
+      IF (cnt_down < N - 1) THEN
         cnt_down <= cnt_down + 1;
       ELSE
         cnt_down <= 0;
